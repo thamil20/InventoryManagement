491cd7c8ded2b296b49087c95d8949428c49043b
Tyler <hamiltontyler3900@icloud.com>
2025-08-03 11:22:33 -0400

    removed try and except when unnecessary and cleaned up logger messages

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 # PyQT5 Intro
 import sys
 from PyQt5.QtGui import QIcon, QFontDatabase
-from PyQt5.QtWidgets import (QApplication, QMainWindow, QStackedWidget)
-from logs.logger import Logger
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QLabel,
+                             QPushButton, QWidget, QVBoxLayout, QStackedWidget)
 from pages.mainMenu import MainMenu
 from pages.addInventory import AddInventory
 #from pages.currentInventory import CurrentInventory
@@ -19,36 +20,37 @@ class MainWindow(QMainWindow):
         self.screen_height = QApplication.primaryScreen().availableGeometry().height()
         self.setGeometry(0, 0, self.screen_width, self.screen_height)
 
-        self.logger = Logger("./logs/log.log")
         # Custom Font Setup
-        self.customFonts = ["./fonts/DancingScript-VariableFont_wght.ttf", ]
-        for font in self.customFonts:
-            if QFontDatabase.addApplicationFont(font) == -1:
-                self.logger.errorLog("Custom font failed to load")
-            else:
-                self.fonts = QFontDatabase.applicationFontFamilies(QFontDatabase.addApplicationFont(font))
-                self.logger.infoLog("Fonts loaded")
+        try:
+            self.customFonts = ["fonts/DancingScript-VariableFont_wght.ttf", ]
+            for font in self.customFonts:
+                if QFontDatabase.addApplicationFont(font) == -1:
+                    self.logger.debugLog("Font Failed To Load.")
+                else:
+                    self.fonts = QFontDatabase.applicationFontFamilies(QFontDatabase.addApplicationFont(font))
             self.currentFont = self.fonts[0]
+        except Exception as e:
+            self.currentFont = "Times"
+            self.logger.debugLog(e)
 
         # Create the stack
         self.stackedWidget = QStackedWidget(self)
         self.setCentralWidget(self.stackedWidget)
 
         # Add all page widgets to the stack
-        try:
-            self.stackedWidget.addWidget(MainMenu(self.addInventoryCallback)) # index 0
-            self.stackedWidget.addWidget(AddInventory(self.cancelCallback)) # index 1
-            # self.stackedWidget.addWidget(CurrentInventory()) # index 2
-            # self.stackedWidget.addWidget(SoldInventory()) # index 3
-            # self.stackedWidget.addWidget(Income()) # index 4
-        except Exception as e:
-            self.logger.debugLog(e)
+        self.stackedWidget.addWidget(MainMenu(self.addInventoryCallback)) # index 0
+        self.stackedWidget.addWidget(AddInventory(self.cancelCallback)) # index 1
+        # self.stackedWidget.addWidget(CurrentInventory()) # index 2
+        # self.stackedWidget.addWidget(SoldInventory()) # index 3
+        # self.stackedWidget.addWidget(Income()) # index 4
+
         # Go to main menu index
         self.stackedWidget.setCurrentIndex(0)
 
     # Used to cancel current action
     def cancelCallback(self):
         self.stackedWidget.setCurrentIndex(0)
+
     # Used to go to the add inventory page
     def addInventoryCallback(self):
         self.stackedWidget.setCurrentIndex(1)
