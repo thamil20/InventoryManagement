8ba809b688c0cb15110d876d8494dc811a20f3bc
Tyler Hamilton <hamiltontyler3900@icloud.com>
2025-08-06 13:15:39 -0400

    Began work on CurrentInventory

--- pages/currentInventory.py
+++ pages/currentInventory.py
@@ -1,2 +1,20 @@
 # TODO
-# Working here next
+from PyQt5 import Qt
+from PyQt5.QtCore import pyqtSignal, QByteArray
+from PyQt5.QtWidgets import QWidget, QListWidget, QPushButton, QListView, QLayout, QVBoxLayout
+from PyQt5.QtSql import QSqlTableModel, QSqlQueryModel
+from PyQt5.QtGui import QPixmap, QImageReader, QImage, QFontDatabase, QFont
+from logs.logger import Logger
+from logic.databaseLogic import connectToDB, getInfo
+
+# Fetches and prints results
+conn = connectToDB("postgres")
+results = getInfo(conn)
+for tup in results:
+    for item in tup:
+        print(item)
+
+class CurrentInventory(QWidget):
+    pageChangeRequest = pyqtSignal(str)
+
+
